409353347e8958fb5afe2ac7447293606fcb9169
PCWitcher <pcwitcher.ie@gmail.com>
2019-08-25 13:49:57 +0100

    accordian

--- src/js/script.js
+++ src/js/script.js
@@ -60,8 +60,7 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
-
-      console.log('new Product:', thisProduct);
+      thisProduct.initAccordion();
     }
 
     renderInMenu() {
@@ -79,12 +78,42 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+
+    initAccordion() {
+      const thisProduct = this;
+      //console.log('thisProduct:', thisProduct);
+
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      console.log('clickableTrigger:', clickableTrigger);
+      /* START: click event listener to trigger */
+      clickableTrigger.addEventListener('click', function (event) {
+        console.log('clicked');
+        /* prevent default action for event */
+        event.preventDefault();
+        /* toggle active class on element of thisProduct */
+        thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+        console.log('thisProduct:', thisProduct);
+        /* find all active products */
+        const allActiveProducts = document.querySelectorAll(select.all.menuProductsActive);
+        /* START LOOP: for each active product */
+        for (let activeProduct of allActiveProducts) {
+          /* START: if the active product isn't the element of thisProduct */
+          if (activeProduct !== thisProduct.element) {
+            /* remove class active for the active product */
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+            /* END: if the active product isn't the element of thisProduct */
+          }
+          /* END LOOP: for each active product */
+        }
+        /* END: click event listener to trigger */
+      });
+    }
   }
 
   const app = {
     initMenu: function () {
       const thisApp = this;
-      console.log('thisApp.data:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
